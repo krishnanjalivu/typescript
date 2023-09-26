a0c6fda4d8691f891fafa081a4d9fad167c2b81e
krishnanjalivu <vukrishnanjali@gmail.com>
2023-09-26 20:40:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var age = 20; //we cannot set it another data type
+//with typescript you can find most of the errors in compile time not in run time
---
+++ index.ts
@@ -0,0 +1,4 @@
+let age:number=20; //we cannot set it another data type
+//with typescript you can find most of the errors in compile time not in run time
+
+
